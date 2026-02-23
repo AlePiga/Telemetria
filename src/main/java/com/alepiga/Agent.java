2aeea7ace829fe26a51da15fcd49224d659c9bf6
AlePiga <134812981+AlePiga@users.noreply.github.com>
2026-02-23 16:55:38 +0100

    dio merda

--- src/main/java/com/alepiga/Agent.java
+++ src/main/java/com/alepiga/Agent.java
@@ -1,4 +1,42 @@
 package com.alepiga;
 
-public class Client {
+import oshi.SystemInfo;
+import oshi.hardware.GlobalMemory;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import static java.net.InetAddress.getByName;
+
+public class Agent {
+    public static void main(String[] args) {
+        try {
+            while(true){
+                // Variabili della telemetria
+                SystemInfo si =  new SystemInfo();
+                GlobalMemory gmemory = si.getHardware().getMemory();
+                long totalMemory = gmemory.getTotal();
+                long usedMemory = gmemory.getTotal() - gmemory.getAvailable();
+                String os = si.getOperatingSystem().toString(); // Per stampare tutti i dettagli sul Sistema Operativo
+                long time = System.currentTimeMillis();
+
+                // Creo una stringa e i byte che la compongono
+                String s = time + ";" + totalMemory + ";" + usedMemory + ";" + os;
+                byte[] buffer = s.getBytes();
+
+                // Invio il pacchetto
+                InetAddress address = getByName("127.0.0.1");
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 9000);
+                DatagramSocket socket = new DatagramSocket();
+                socket.send(packet);
+
+                // Attende cinque secondi
+                Thread.sleep(5000);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/alepiga/Server.java
+++ src/main/java/com/alepiga/Server.java
@@ -1,4 +1,63 @@
 package com.alepiga;
+import de.vandermeer.skb.interfaces.Sleep;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Server {
+    // Processo Main che avvia il thread
+    public static void main(String[] args) {
+        try{
+            DatagramSocket socket = new DatagramSocket(9000); // Creo un nuovo socket UDP
+            while(true){
+                DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+                socket.receive(packet);
+                new Thread(() -> handleClient(packet)).start();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    // Per convertire i tick della RAM in GB
+    public static double convertiGB(long valore){
+        double divisore = Math.pow(1024, 3);
+        return (double)valore / divisore;
+    }
+
+    // Per convertire i millisecondi dal 1/1/1970 in stringa
+    public static String timestampToString(long timestamp) {
+        LocalDateTime dateTime = Instant.ofEpochMilli(timestamp)
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        return dateTime.format(formatter);
+    }
+
+    // Thread principale, riceve e stampa i pacchetti
+    public static void handleClient(DatagramPacket packet){
+        // Creazione stringa
+        String s = new String(packet.getData(), 0, packet.getLength());
+        String[] parts = s.split(";");
+
+        // Dati parsati dall'agent
+        long time = Long.parseLong(parts[0]);
+        long totalMemory = Long.parseLong(parts[1]);
+        long usedMemory = Long.parseLong(parts[2]);
+        String os = parts[3];
+        String sFinal = String.format(
+                "Timestamp: %s\nMemoria totale: %.2f GB\nMemoria usata: %.2f GB\nSistema operativo: %s\n",
+                timestampToString(time),
+                convertiGB(totalMemory),
+                convertiGB(usedMemory),
+                os
+        );
+        System.out.println(sFinal); // Stampa stringa finale
+
+    }
 }
